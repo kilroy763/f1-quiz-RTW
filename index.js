69759ba92722bc09d708bb037ca1a62f203c3d7e
kilroy763 <itzmaxkiljan@hotmail.com>
2021-04-13 16:37:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const request = require('request');
 const path = require('path');
-const port = 5000;
+const port = 8000;
 const render = require('./modules/render.js');
 const fetcher = require('./modules/fetch.js');
 
